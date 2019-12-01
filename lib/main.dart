2ec8aadc4056c31821133edf43546127bf105fe0
luís fernando <munhozfontana@gmail.com>
2019-12-01 17:38:44 -0300

    configurando firebase

---
+++ lib/main.dart
@@ -0,0 +1,13 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
