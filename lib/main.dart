5025e42bef8ffafc494cf6250c4a4dfcc91eb455
luís fernando <munhozfontana@gmail.com>
2019-12-01 17:54:25 -0300

    inicio ao leyout do chat

--- lib/main.dart
+++ lib/main.dart
@@ -5,9 +5,50 @@ void main() {
   runApp(MyApp());
 }
 
+final ThemeData kIOSTheme = ThemeData(
+    primarySwatch: Colors.orange,
+    primaryColor: Colors.green[100],
+    primaryColorBrightness: Brightness.light);
+
+final ThemeData kDefaultTheme = ThemeData(
+  primarySwatch: Colors.purple,
+  accentColor: Colors.orangeAccent[400],
+);
+
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return MaterialApp(
+      title: "Chat App",
+      debugShowCheckedModeBanner: false,
+      theme: Theme.of(context).platform == TargetPlatform.iOS
+          ? kIOSTheme
+          : kDefaultTheme,
+      home: ChatScreen(),
+    );
+  }
+}
+
+class ChatScreen extends StatefulWidget {
+  @override
+  _ChatScreenState createState() => _ChatScreenState();
+}
+
+class _ChatScreenState extends State<ChatScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return SafeArea(
+      bottom: true,
+      top: true,
+      child: Scaffold(
+        appBar: AppBar(
+          title: Text("Chat App"),
+          centerTitle: true,
+          elevation:
+              Theme.of(context).platform == TargetPlatform.iOS ? 0.0 : 4.0,
+        ),
+        body: Column(),
+      ),
+    );
   }
 }
